21aad22eb4aff9efba0713dbe653d8996eefdd7f
rushidinesh <rdpatel30@myseneca.ca>
2020-09-28 16:31:51 -0400

    second commit

--- server.js
+++ server.js
@@ -17,9 +17,13 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 // POST /api/sales (NOTE: This route must read the contents of the request body)
 app.post("/api/sales", (req, res)=>{
-    res.json(11).json(myData.addNewSale(req.body)).catch((err)=>{
-        console.log(err);
-    });
+    myData.addNewSale(req.body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(`Error found: ${err}`);
+        });
   });
 
 
@@ -32,33 +36,33 @@ app.get("/api/sales", (req, res) => {
       }).catch((err)=>{
        res.json(err);
     });
-      
   });
 
 
 // GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
 
 app.get("/api/sales/:id", (req, res) => {
-    myData.getSaleById(req.params.ab).then((data) => {
-        res.json(10).json({ data });
-      });
-      
-  });
-
+    myData.getSaleById(req.params.id).then((data)=>{
+        res.json(data);
+    })
+   });
 // PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body)
 
 app.put("/api/sales/:id", (req, res) => {
-    myData.updateSaleById(req.body, req.params.ab).then((data) => {
-        res.json(10).json({ message: data });
-      });
+    myData.updateSaleById(req.body, req.params.id).then((data)=>{
+        res.json({message:"Data add complete"});
+    })
+    .catch((err)=>{
+        res.json(err);
+    });
       
   });
 
 // DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
 app.delete("/api/sales/:id", (req, res) => {
-    myData.deleteSaleById(req.params.ab).then((data) => {
-        res.json(10).json({ message: data });
-      });
+    myData.deleteSaleById(req.params.id).then((data)=>{
+        res.json(data);
+    })
       
   });
 
@@ -71,4 +75,3 @@ myData.initialize().then(()=>{
 }).catch((err)=>{
     console.log(err);
 });
-
